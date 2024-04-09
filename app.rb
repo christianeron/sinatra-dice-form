0d259c107a741a729477d759a6442c7b1f4a7266
christianeron <christianeron1@gmail.com>
2024-04-09 21:29:13 +0000

    all tests passed

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
 require "sinatra"
 require "sinatra/reloader"
 
+#rake grade: 5hYUzTQhiti4tuCgc5ihzrW6
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:root)
+end
+
+get("/process_roll") do
+  @rolls = []
+  @num_rolls = params.fetch("dice").to_i
+  @num_sides = params.fetch("sides").to_i
+  @num_rolls.times do
+    die = rand(1..@num_sides)
+    @rolls.push(die)
+  end
+erb(:results)
 end
